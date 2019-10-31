a3ff9d635af119d324e7a7dd58468103c664edda
rcobian <rcobian29@yahoo.com>
2019-10-31 15:30:14 -0700

    Changes 10/31

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -4,7 +4,6 @@ from BoardClasses import Board
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
 class StudentAI():
-
     def __init__(self,col,row,p):
         self.col = col
         self.row = row
@@ -14,6 +13,37 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
+
+    def num_pieces(self):
+        color = self.opponent[self.color]
+        if color == 1:
+            color = 'B'
+            openent_pieces = self.board.black_count
+        elif color == 2:
+            color = 'W'
+            openent_pieces = self.board.white_count
+        return openent_pieces
+
+    def get_move(self,move):
+        if len(move) != 0:
+            self.board.make_move(move,self.opponent[self.color])
+        else:
+            self.color = 1
+        moves = self.board.get_all_possible_moves(self.color)
+        openent_pieces = self.num_pieces()
+        for i,checker in enumerate(moves):
+            for j,move in enumerate(moves[i]):
+                self.board.make_move(moves[i][j], self.color) #Make move
+                temp = self.num_pieces()
+                if temp < openent_pieces: # check if move took openent piece
+                    openent_pieces = temp
+                    best = (i,j)
+                else:
+                    best = (0,0)
+                self.board.undo()
+        self.board.make_move(moves[best[0]][best[1]],self.color)
+
+'''
     def get_move(self,move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
@@ -25,3 +55,4 @@ class StudentAI():
         move = moves[index][inner_index]
         self.board.make_move(move,self.color)
         return move
+'''
